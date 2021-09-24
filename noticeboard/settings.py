0c0280b47eb870a65f88452627ac175d085f56e6
vinchuli <mavenwechuli@gmail.com>
2021-09-24 03:46:33 +0300

    removing static jquery-ui.min.css commented imagefiles

--- noticeboard/settings.py
+++ noticeboard/settings.py
@@ -123,6 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 ALLOWED_HOSTS = ["*"]
+# PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+# STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 MEDIA_URL="/media/"
 MEDIA_ROOT=os.path.join(BASE_DIR,"media")
